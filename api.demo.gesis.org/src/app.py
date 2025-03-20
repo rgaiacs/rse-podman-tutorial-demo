ebb9fa7863af7188ab245b70bc677bbf952699a6
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2025-03-20 17:46:55 +0100

    Work around for CORS

--- api.demo.gesis.org/src/app.py
+++ api.demo.gesis.org/src/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask.json import jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return {
+    response = jsonify({
         "msg": "Hello world!"
-    }
+    })
+    # This is unsafe. Do NOT use it!
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
